d6b6dfb228872b872d880e6d3f65a7237a377152
Eugene Medvedev <eugenemedvedevbelarus@gmail.com>
2021-11-10 18:46:56 +0300

    match for all responses

--- script.js
+++ script.js
@@ -1,67 +1,68 @@
 import axios from "axios";
 import xl from 'excel4node'
 
-async function main(...args) {
-
-    let responses = []
-    let dirtyParams = []
-    let cleanParams = []
-    let promises = args.map(url => getData(url, responses))
+async function main(...uris) {
+    const hosts = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002']
+    let promises = []
+    let requestsUrls = []
+    hosts.map(host => uris.map(uri => requestsUrls.push(host+uri)))
+    promises = requestsUrls.map(url => getData(url))
     Promise.all(promises)
-        .then(res => {
-            res.map(e => console.log(e.data))
-            // responses = res[0].responses
+        .then(responsesArr => {
+            let responses = []
+            let dirtyParams = []
+            let cleanParams = []
+            let requests = []
+            let isMatch = false
+            responsesArr.map(e => {
+                requests.push(e.config.url)
+                e.data.length ? responses.push(e.data) : responses.push(['Пустой ответ'])
+                dirtyParams.push(e.config.url.split('?')[1])
+            })
 
+            // pushing to the clean params arr
+            dirtyParams.map(e => cleanParams.push(e.split("&").join(', ')))
 
-            // for (let i = 0; i < args.length; i++) {
-            //     dirtyParams.push(args[i].split("?")[1])
-            // }
-            // dirtyParams.map(e => cleanParams.push(e.split('&')))
-            // excel(args, responses, cleanParams)
+            // checking match for all
+            for (let i = 0; i<responses.length; i++) {
+                responses[0].toString() == responses[i].toString() ? isMatch = true : isMatch = false
+            }
 
-        }
-        )
+            excel(uris, responses, cleanParams, isMatch, requests)
+        })
 }
 
-async function getData(url, responses) {
+async function getData(url) {
     return await axios.get(url)
-        // .then(res => res.data.length ? responses.push(res.data) : responses.push(["Пустой ответ"]))
-    // return { responses }
 }
 
-function excel(args, responses, cleanParams) {
+function excel(uris, responses, cleanParams, isMatch, requests) {
 
     let wb = new xl.Workbook()
     let ws = wb.addWorksheet('Response checker')
-
-    let isMatch = false
-
-    if (args.length > 1) {
-        for (let i = 0; i < args.length; i++) {
-            responses[0].toString() == responses[i].toString() ? isMatch = true : isMatch = false
-        }
-    }
 
     //set columns width
     ws.column(1).setWidth(50)
     ws.column(2).setWidth(40)
     ws.column(3).setWidth(40)
     ws.column(4).setWidth(20)
+    ws.column(5).setWidth(20)
 
     //set names to the columns
     ws.cell(1, 1).string('Url request string')
-    ws.cell(1, 2).string('Url params')
+    ws.cell(1, 2).string('Uri params')
     ws.cell(1, 3).string('Response')
     ws.cell(1, 4).string('Match for all')
-
+    ws.cell(1, 5).string('Match by params')
+    console.log(requests)
     //filling url params column by cycle
-    for (let i = 2; i < cleanParams.length + 2; i++) {
-        ws.cell(i, 2).string(`${cleanParams[i - 2].join(', ')}`)
+    for(let i = 2; i<requests.length+2; i++) {
+        ws.cell(i, 1).string(requests[i-2])
     }
 
-    //filling url request string column 
-    for (let i = 2; i < args.length + 2; i++) {
-        ws.cell(i, 1).string(`${args[i - 2]}`)
+    //filling uri params string column 
+    for (let i = 2; i < cleanParams.length + 2; i++) {
+        ws.cell(i, 2).string(`${cleanParams[i - 2]}`)
     }
 
     //filling responses column
@@ -75,4 +76,4 @@ function excel(args, responses, cleanParams) {
     wb.write('excel.xlsx')
 }
 
-main('http://localhost:3000/api?page=1&stream=false', 'http://localhost:3000/api?page=1&stream=true', 'http://localhost:3000/api?page=2&stream=false')
+main('/api?page=1&stream=false', '/api?page=1&stream=true', '/api?page=2&stream=false')
