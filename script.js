7aeb9652dbbaa2ac28336e41299d19e9d0331ec9
Eugene Medvedev <eugenemedvedevbelarus@gmail.com>
2021-11-15 17:41:11 +0300

    add all neaded features and excel parsing

--- script.js
+++ script.js
@@ -1,110 +1,126 @@
 import axios from "axios";
 import xl from 'excel4node'
+import fs from "fs"
+import xlsx from 'xlsx'
 
-async function main(...uris) {
-    const hosts = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002']
-    let promises = []
-    let requestsUrls = []
-    requestsUrls.push(hosts.map(host => uris.map(uri => host + uri)))
-    requestsUrls = requestsUrls.flat(Infinity)
-    promises = requestsUrls.map(url => getData(url))
+const excelFile = xlsx.readFile('./excel.xlsx')
+const sheet = excelFile.Sheets[excelFile.SheetNames[0]]
+
+const config = [
+    // {
+    //     host: 'http://localhost:3000',
+    //     headers: {
+    //         "Authorization": "Bearer dsadsdas"
+    //     }
+    // },
+    // {
+    //     host: 'http://localhost:3001',
+    //     headers: {
+    //         "Authorization": "Bearer 321321312"
+    //     }
+    // },
+    // {
+    //     host: 'http://localhost:3002',
+    //     headers: {
+    //         "Authorization": "Bearer d21312dsa"
+    //     }
+    // },
+]
+const uris = []
+const hosts = []
+const headers = []
+for(let i = 2; i > 0; i++) {
+    if (!sheet['A'+i]) break
+    config.push({host: sheet['A'+i].v, headers: sheet["B"+i].v})
+    uris.push(sheet["C"+i].v)
+    headers.push(sheet["B"+i].v)
+    hosts.push(sheet["A"+i].v)
+}
+
+
+async function main() {
+    let requests = config.map(req => uris.map(uri => ({url: req.host + uri, headers: req.headers, uri: uri})))
+    requests = requests.flat(1)
+    const promises = requests.map(el => getData(el.url, el.headers))
     Promise.all(promises)
         .then(responsesArr => {
             let responses = []
-            let dirtyParams = []
-            let cleanParams = []
-            let requests = []
-            let resByUri = []
-            let isMatch = false
 
-            uris.map(e => resByUri.push({params: e.split('?')[1], data: [], match: true}))
-
-            for(let i = 0; i<responsesArr.length; i++) {
-                requests.push(responsesArr[i].config.url)
-                responsesArr[i].data.length ? responses.push(responsesArr[i].data) : responses.push(['Пустой ответ'])
-                dirtyParams.push(responsesArr[i].config.url.split('?')[1])
-
+            const resByUri = uris.map(e => ({ params: e.split('?')[1], data: [], match: true }))
+            for (let i = 0; i < responsesArr.length; i++) {
+                requests[i].response = responsesArr[i].data.length ? responsesArr[i].data : "Empty response"
+                responses.push(JSON.stringify(responsesArr[i].data))
             }
 
             for (let i = 0; i < responses.length; i++) {
-                resByUri[i%uris.length].data.push(responses[i].toString())
+                resByUri[i % uris.length].data.push(responses[i].toString())
             }
-            
-            for(let i = 0; i < resByUri.length; i++) {
+
+            for (let i = 0; i < resByUri.length; i++) {
                 let match = false
                 // "испачкал" респонсы чтобы проверить проверку
-                resByUri[1].data[1] = ''
-                resByUri[i].data.forEach((e, o) => 
-                    {
-                        match = (resByUri[i].data[o+1] ? e == resByUri[i].data[o+1] : e == resByUri[i].data[0])
-                        if (!match) resByUri[i].match = false
-                        if (match && !(resByUri[i].match == false)) resByUri[i].match = true
-                    }
-                )
+                // resByUri[1].data[1] = ''
+                resByUri[i].data.forEach((e, o) => {
+                    match = (resByUri[i].data[o + 1] ? e == resByUri[i].data[o + 1] : e == resByUri[i].data[0])
+                    if (!match) resByUri[i].match = false
+                    if (match && !(resByUri[i].match == false)) resByUri[i].match = true
+                })
             }
 
-            // pushing to the clean params arr
-            for (let i =0; i< dirtyParams.length; i++) {
-                cleanParams.push(dirtyParams[i].split("&").join(', '))
-            }
-            // dirtyParams.map(e => cleanParams.push(e.split("&").join(', ')))
-
-            // checking match for all
-            for (let i = 0; i < responses.length; i++) {
-                responses[0].toString() == responses[i].toString() ? isMatch = true : isMatch = false
-            }
-
-            excel(responses, cleanParams, isMatch, requests, resByUri)
+            writeExcel(requests, resByUri)
         })
 }
 
-function getData(url) {
-    return axios.get(url)
+function getData(url, headers) {
+    console.log(url)
+    return axios.get(url, {
+        headers: JSON.parse(headers)
+    })
 }
 
-function excel(responses, cleanParams, isMatch, requests, resByUri) {
-
+function writeExcel(requests, resByUri) {
     let wb = new xl.Workbook()
     let ws = wb.addWorksheet('Response checker')
 
     //set columns width
     ws.column(1).setWidth(50)
-    ws.column(2).setWidth(40)
+    ws.column(2).setWidth(50)
     ws.column(3).setWidth(40)
-    ws.column(4).setWidth(20)
-    ws.column(5).setWidth(20)
+    ws.column(4).setWidth(40)
+    ws.column(6).setWidth(20)
 
     //set names to the columns
-    ws.cell(1, 1).string('Url request string')
-    ws.cell(1, 2).string('Uri params')
-    ws.cell(1, 3).string('Response')
-    ws.cell(1, 4).string('Match for all')
-    ws.cell(1, 5).string('Match by params')
+    ws.cell(1, 1).string('Hosts')
+    ws.cell(1, 2).string('Headers')
+    ws.cell(1, 3).string('Uri params')
+    ws.cell(1, 4).string('Response')
+    ws.cell(1, 6).string('Match by params')
 
-    //filling url params column by cycle
-    for (let i = 2; i < requests.length + 2; i++) {
-        ws.cell(i, 1).string(requests[i - 2])
+    //filling hosts column by cycle
+    for (let i = 2; i < hosts.length + 2; i++) {
+        ws.cell(i, 1).string(hosts[i - 2])
     }
 
-    //filling uri params string column 
-    for (let i = 2; i < cleanParams.length + 2; i++) {
-        ws.cell(i, 2).string(`${cleanParams[i - 2]}`)
+    for (let i = 2; i < headers.length + 2; i++) {
+        ws.cell(i, 2).string(headers[i-2])
+    }
+
+    // // filling uri params string column 
+    for (let i = 2; i < uris.length + 2; i++) {
+        ws.cell(i, 3).string(uris[i-2])
     }
 
     //filling responses column
-    for (let i = 2; i < responses.length + 2; i++) {
-        ws.cell(i, 3).string(responses[i - 2].toString())
+    for (let i = 2; i < requests.length + 2; i++) {
+        ws.cell(i, 4).string(JSON.stringify(requests[i - 2].response))
     }
 
-    //filling match column
-    isMatch ? ws.cell(2, 4).string('Ответы совпали') : ws.cell(2, 4).string('Ответы не совпали')
-
     for (let i = 2; i < resByUri.length + 2; i++) {
-        ws.cell(i, 5).string(resByUri[i-2].params)
-        ws.cell(i, 6).string(resByUri[i-2].match.toString())
+        ws.cell(i, 6).string(resByUri[i - 2].params)
+        ws.cell(i, 7).string(resByUri[i - 2].match.toString())
     }
 
     wb.write('excel.xlsx')
 }
 
-main('/api?page=1&stream=false', '/api?page=1&stream=true', '/api?page=2&stream=false')
+main()
