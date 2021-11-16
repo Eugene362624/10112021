d2fd89a244525f9915bfe78a19807a49903d8f8f
Eugene Medvedev <eugenemedvedevbelarus@gmail.com>
2021-11-16 15:50:59 +0300

    edit script principles. add app status info

--- script.js
+++ script.js
@@ -5,35 +5,43 @@ import xlsx from 'xlsx'
 const excelFile = xlsx.readFile('./excel.xlsx')
 const sheet = excelFile.Sheets[excelFile.SheetNames[0]]
 
-const config = []
-const uris = []
-const hosts = []
-const headers = []
+const config = {
+    hosts: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    headers: [
+        {Authorization: "Bearer AAassdasd"}, 
+        {Authorization: "Bearer AA442321"}, 
+        {Authorization: "Bearer 42763271261"}
+    ],
+    uris: [],
+    requests: []
+}
 
+// parsing excel uris column
 for(let i = 2; i > 0; i++) {
     if (!sheet['A'+i]) break
-    config.push({host: sheet['A'+i].v, headers: sheet["B"+i].v})
-    uris.push(sheet["C"+i].v)
-    headers.push(sheet["B"+i].v)
-    hosts.push(sheet["A"+i].v)
+    config.uris.push(sheet["A"+i].v)
 }
 
 async function main() {
-    let requests = config.map(req => uris.map(uri => ({url: req.host + uri, headers: req.headers, uri: uri})))
-    requests = requests.flat(1)
-    const promises = requests.map(el => getData(el.url, el.headers))
+    if (config.hosts.length !== config.headers.length) {
+        console.log('Error in config. Invalid hosts or headers length.')
+        return
+    }
+    config.requests = config.hosts.map((host, e) => config.uris.map(uri => ({url: host + uri, headers: config.headers[e]})))
+    config.requests = config.requests.flat(1)
+    const promises = config.requests.map(el => getData(el.url, el.headers))
     Promise.all(promises)
         .then(responsesArr => {
             let responses = []
+            const resByUri = config.uris.map(e => ({ params: e.split('?')[1], data: [], match: true }))
 
-            const resByUri = uris.map(e => ({ params: e.split('?')[1], data: [], match: true }))
             for (let i = 0; i < responsesArr.length; i++) {
-                requests[i].response = responsesArr[i].data.length ? responsesArr[i].data : "Empty response"
-                responses.push(JSON.stringify(responsesArr[i].data))
+                responses.push(responsesArr[i].data.length ? JSON.stringify(responsesArr[i].data) : "Empty response")
+                config.requests[i].response = responsesArr[i].data.length ? JSON.stringify(responsesArr[i].data) : "Empty response"
             }
 
             for (let i = 0; i < responses.length; i++) {
-                resByUri[i % uris.length].data.push(responses[i].toString())
+                resByUri[i % config.uris.length].data.push(responses[i].toString())
             }
 
             for (let i = 0; i < resByUri.length; i++) {
@@ -45,61 +53,49 @@ async function main() {
                 })
             }
 
-            writeExcel(requests, resByUri)
+            writeExcel(config, resByUri)
         })
 }
 
 function getData(url, headers) {
-    console.log(url)
     return axios.get(url, {
-        headers: JSON.parse(headers)
+        headers: headers
     })
 }
 
-function writeExcel(requests, resByUri) {
+function writeExcel(config, resByUri) {
     let wb = new xl.Workbook()
     let ws = wb.addWorksheet('Response checker')
 
     //set columns width
-    ws.column(1).setWidth(50)
-    ws.column(2).setWidth(50)
-    ws.column(3).setWidth(40)
-    ws.column(4).setWidth(40)
-    ws.column(6).setWidth(20)
+    ws.column(1).setWidth(30)
+    ws.column(2).setWidth(20)
+    // ws.column(3).setWidth(50)
 
     //set names to the columns
-    ws.cell(1, 1).string('Hosts')
-    ws.cell(1, 2).string('Headers')
-    ws.cell(1, 3).string('Uri params')
-    ws.cell(1, 4).string('Response')
-    ws.cell(1, 6).string('Match by params')
+    ws.cell(1, 1).string('Uri params')
+    ws.cell(1, 2).string('Is match')
+    ws.cell(1, 3).string('Responses')
 
-    //filling hosts column by cycle
-    for (let i = 2; i < hosts.length + 2; i++) {
-        ws.cell(i, 1).string(hosts[i - 2])
+    //filling uri column
+    for (let i = 2; i < config.uris.length + 2; i++) {
+        ws.cell(i, 1).string(config.uris[i - 2])
     }
 
-    //filling headers column 
-    for (let i = 2; i < headers.length + 2; i++) {
-        ws.cell(i, 2).string(headers[i-2])
-    }
-
-    // // filling uri params string column 
-    for (let i = 2; i < uris.length + 2; i++) {
-        ws.cell(i, 3).string(uris[i-2])
+    //filling isMatch column
+    for (let i = 2; i < resByUri.length + 2; i++) {
+        ws.cell(i, 2).string(resByUri[i - 2].match.toString().toUpperCase())
     }
 
     //filling responses column
-    for (let i = 2; i < requests.length + 2; i++) {
-        ws.cell(i, 4).string(JSON.stringify(requests[i - 2].response))
-    }
-
     for (let i = 2; i < resByUri.length + 2; i++) {
-        ws.cell(i, 6).string(resByUri[i - 2].params)
-        ws.cell(i, 7).string(resByUri[i - 2].match.toString())
+        for (let e = 3; e < resByUri[0].data.length + 3; e++) {
+            ws.cell(i, e).string(resByUri[i-2].data[e - 3])
+        }
     }
 
     wb.write('excel.xlsx')
+    console.log('Excel is writed.')
 }
 
 main()
